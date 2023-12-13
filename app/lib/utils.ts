c91e4f531ff237d14e7bf8be759f254522d8f681
hsglc <huseyingulcuu@gmail.com>
2023-12-13 16:57:12 +0300

    feat: Add a new util function to get formatted time to use it in delete form component.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,5 +1,5 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
@@ -12,18 +12,35 @@ export function formatRelativeTime(specifiedDate: Date): string {
 
 	const minutesDifference = Math.floor(timeDifference / (1000 * 60));
 
-	const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
+	const rtf = new Intl.RelativeTimeFormat("en", { numeric: "auto" });
 	let formattedTime: string;
 
 	if (minutesDifference < 60) {
-		formattedTime = rtf.format(-minutesDifference, 'minute');
+		formattedTime = rtf.format(-minutesDifference, "minute");
 	} else if (minutesDifference < 60 * 24) {
 		const hoursDifference = Math.floor(minutesDifference / 60);
-		formattedTime = rtf.format(-hoursDifference, 'hour');
+		formattedTime = rtf.format(-hoursDifference, "hour");
 	} else {
 		const daysDifference = Math.floor(minutesDifference / (60 * 24));
-		formattedTime = rtf.format(-daysDifference, 'day');
+		formattedTime = rtf.format(-daysDifference, "day");
 	}
 
 	return formattedTime;
+}
+
+export function getCurrentFormattedDate() {
+	// return in this format: Friday, February 10, 2023 at 5:57 PM
+	const options: Intl.DateTimeFormatOptions = {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+		hour: "numeric",
+		minute: "numeric",
+		hour12: true,
+	};
+
+	const currentDate: string = new Date().toLocaleDateString("en-US", options);
+
+	return currentDate;
 }
