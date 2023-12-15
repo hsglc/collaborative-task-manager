43f91805c33a7ce5f9a855837b82c56ae53c1e3a
hsglc <huseyingulcuu@gmail.com>
2023-12-16 01:23:04 +0300

    fix: Remove unused import.

--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { fetchFriends } from "@/app/features/friendship/actions/fetchFriends";
-
 import { FriendsContainer } from "@/app/features/friendship/containers/FriendsContainer";
 
 export default async function Friends() {
