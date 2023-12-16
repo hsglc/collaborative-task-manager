dbf72599c1bcd415b0f0532aacf4f1cfce95598d
hsglc <huseyingulcuu@gmail.com>
2023-12-16 20:21:51 +0300

    fix: remove unnecessary fields.

--- types/friends.ts
+++ types/friends.ts
@@ -3,14 +3,11 @@ type UserProfile = {
 	username: string | null;
 	avatar_url: string;
 	full_name: string;
-    user_email: string;
+	user_email: string;
 };
 
 export type Friendship = {
-	id: number;
-	created_at_: string;
 	user_id: string;
-	friend_id: string;
 	status: string;
 	profiles: UserProfile;
 };
