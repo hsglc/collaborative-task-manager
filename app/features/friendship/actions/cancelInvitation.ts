6af2dc05ddd7dca02a30474a1d2c7d2e0be92439
hsglc <huseyingulcuu@gmail.com>
2023-12-16 20:19:59 +0300

    feat: Create the cancel invitation server action.

---
+++ app/features/friendship/actions/cancelInvitation.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { CookieOptions, createServerClient } from "@supabase/ssr";
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
+
+export async function cancelInvitation(id: string) {
+	const cookieStore = cookies();
+
+	const supabase = createServerClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		{
+			cookies: {
+				get(name: string) {
+					return cookieStore.get(name)?.value;
+				},
+				set(name: string, value: string, options: CookieOptions) {
+					cookieStore.set({ name, value, ...options });
+				},
+				remove(name: string, options: CookieOptions) {
+					cookieStore.set({ name, value: "", ...options });
+				},
+			},
+		},
+	);
+
+	const { error } = await supabase.from("friends").delete().eq("friend_id", id);
+
+	if (!error) {
+		revalidatePath("/friends");
+		return {
+			message: "Invitation canceled",
+		};
+	}
+	return {
+		message: "Failed to cancel invitation",
+	};
+}
