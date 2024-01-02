25a9a8c3adc194fe93ff292992587384677d4a0f
hsglc <huseyingulcuu@gmail.com>
2024-01-02 22:57:30 +0300

    fix: handle delete action

--- app/components/shared/Navbar.tsx
+++ app/components/shared/Navbar.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client';
 
-import { Logo } from "@/icons/Logo";
+import { Logo } from '@/icons/Logo';
 import {
 	Button,
 	Link as NextUILink,
@@ -11,36 +11,36 @@ import {
 	NavbarMenu,
 	NavbarMenuItem,
 	NavbarMenuToggle,
-} from "@nextui-org/react";
-import { usePathname } from "next/navigation";
-import React, { useEffect, useState } from "react";
+} from '@nextui-org/react';
+import { usePathname } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
-import { Notifications } from "@/app/components/Notifications";
+import { Notifications } from '@/app/components/Notifications';
 import {
 	Avatar,
 	AvatarFallback,
 	AvatarImage,
-} from "@/app/components/ui/avatar";
-import { Button as CustomButton } from "@/app/components/ui/button";
+} from '@/app/components/ui/avatar';
+import { Button as CustomButton } from '@/app/components/ui/button';
 import {
 	Popover,
 	PopoverContent,
 	PopoverTrigger,
-} from "@/app/components/ui/popover";
-import { Skeleton } from "@/app/components/ui/skeleton";
-import clsx from "clsx";
+} from '@/app/components/ui/popover';
+import { Skeleton } from '@/app/components/ui/skeleton';
+import clsx from 'clsx';
 
-import { Notification } from "@/types/notifications";
-import { User } from "@/types/users";
-import { createBrowserClient } from "@supabase/ssr";
-import Link from "next/link";
+import { Notification } from '@/types/notifications';
+import { User } from '@/types/users';
+import { createBrowserClient } from '@supabase/ssr';
+import Link from 'next/link';
 
-type EventType = "INSERT" | "UPDATE" | "DELETE";
+type EventType = 'INSERT' | 'UPDATE' | 'DELETE';
 
 export function NavigationBar({ user }: { user: User }) {
 	const supabase = createBrowserClient(
 		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 	);
 
 	const [notifications, setNotifications] = useState<Notification[]>([]);
@@ -50,9 +50,9 @@ export function NavigationBar({ user }: { user: User }) {
 			data: { user },
 		} = await supabase.auth.getUser();
 		const { data: notifications } = await supabase
-			.from("notifications")
-			.select("*")
-			.eq("target_id", user?.id);
+			.from('notifications')
+			.select('*')
+			.eq('target_id', user?.id);
 
 		setNotifications(notifications as Notification[]);
 	};
@@ -63,35 +63,39 @@ export function NavigationBar({ user }: { user: User }) {
 
 	useEffect(() => {
 		const channel = supabase
-			.channel("notifications-channel")
+			.channel('notifications-channel')
 			.on(
-				"postgres_changes",
+				'postgres_changes',
 				{
-					event: "*",
-					schema: "public",
-					table: "notifications",
+					event: '*',
+					schema: 'public',
+					table: 'notifications',
 					filter: `target_id=eq.${user?.id}`,
 				},
 				(payload) => {
 					const evenType: EventType = payload.eventType;
-					const notification = payload.new as Notification;
-					console.log("notification : ", payload);
-					if (evenType === "INSERT") {
+					const newNotification = payload.new as Notification;
+					const oldNotification = payload.old as Notification;
+					if (evenType === 'INSERT') {
 						setNotifications((notifications) => [
 							...notifications,
-							notification,
+							newNotification,
 						]);
-					} else if (evenType === "UPDATE") {
+					} else if (evenType === 'UPDATE') {
 						setNotifications((notifications) => [
-							...notifications.filter((nf) => nf.id !== notification.id),
-							notification,
+							...notifications.filter(
+								(nf) => nf.id !== newNotification.id
+							),
+							newNotification,
 						]);
-					} else if (evenType === "DELETE") {
+					} else if (evenType === 'DELETE') {
 						setNotifications((notifications) =>
-							notifications.filter((nf) => nf.id !== notification.id),
+							notifications.filter(
+								(nf) => nf.id !== oldNotification.id
+							)
 						);
 					}
-				},
+				}
 			)
 			.subscribe();
 		return () => {
@@ -101,103 +105,107 @@ export function NavigationBar({ user }: { user: User }) {
 
 	const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-	const menuItems = ["Profile", "Dashboard", "Friends", "Log Out"];
+	const menuItems = ['Profile', 'Dashboard', 'Friends', 'Log Out'];
 
 	const pathname = usePathname();
 
 	return (
-		<Navbar onMenuOpenChange={setIsMenuOpen} maxWidth="full">
+		<Navbar onMenuOpenChange={setIsMenuOpen} maxWidth='full'>
 			<NavbarContent>
 				<NavbarMenuToggle
-					aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-					className="sm:hidden"
+					aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+					className='sm:hidden'
 				/>
 				<NavbarBrand>
 					<Logo />
-					<p className="font-bold text-inherit">COLLABORAMATE</p>
+					<p className='font-bold text-inherit'>COLLABORAMATE</p>
 				</NavbarBrand>
 			</NavbarContent>
 
 			{user ? (
-				<div className="flex-center gap-12">
-					<NavbarContent className="hidden sm:flex gap-4" justify="center">
+				<div className='flex-center gap-12'>
+					<NavbarContent
+						className='hidden sm:flex gap-4'
+						justify='center'>
 						<NavbarItem>
 							<Link
 								className={clsx(
-									"flex h-[48px] grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+									'flex h-[48px] grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
 									{
-										" text-blue-600": pathname === "/friends",
-									},
+										' text-blue-600':
+											pathname === '/friends',
+									}
 								)}
-								href="/friends"
-							>
+								href='/friends'>
 								Friends
 							</Link>
 						</NavbarItem>
 						<NavbarItem isActive>
 							<Link
 								className={clsx(
-									"flex h-[48px] grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+									'flex h-[48px] grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
 									{
-										" text-blue-600": pathname === "/profile",
-									},
+										' text-blue-600':
+											pathname === '/profile',
+									}
 								)}
-								href="/profile"
-							>
+								href='/profile'>
 								Profile
 							</Link>
 						</NavbarItem>
 						<NavbarItem>
 							<Link
 								className={clsx(
-									"flex h-[48px] grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+									'flex h-[48px] grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
 									{
-										" text-blue-600": pathname === "/dashboard",
-									},
+										' text-blue-600':
+											pathname === '/dashboard',
+									}
 								)}
-								href="/dashboard"
-							>
+								href='/dashboard'>
 								Dashboard
 							</Link>
 						</NavbarItem>
 						<NavbarItem>
-							<div className="flex-center gap-3">
+							<div className='flex-center gap-3'>
 								<Popover>
 									<PopoverTrigger asChild>
 										<CustomButton
-											variant="outline"
-											className="relative bg-transparent hover:bg-black group text-black hover:text-white border-2 hover:border-black"
-										>
+											variant='outline'
+											className='relative bg-transparent hover:bg-black group text-black hover:text-white border-2 hover:border-black'>
 											<svg
-												className=" w-5 h-5 group-hover:text-white "
-												fill="none"
-												height="24"
-												stroke="currentColor"
-												strokeLinecap="round"
-												strokeLinejoin="round"
-												strokeWidth="2"
-												viewBox="0 0 24 24"
-												width="24"
-												xmlns="http://www.w3.org/2000/svg"
-											>
-												<path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
-												<path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+												className=' w-5 h-5 group-hover:text-white '
+												fill='none'
+												height='24'
+												stroke='currentColor'
+												strokeLinecap='round'
+												strokeLinejoin='round'
+												strokeWidth='2'
+												viewBox='0 0 24 24'
+												width='24'
+												xmlns='http://www.w3.org/2000/svg'>
+												<path d='M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9' />
+												<path d='M10.3 21a1.94 1.94 0 0 0 3.4 0' />
 											</svg>
 											{notifications.length > 0 ? (
-												<span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1.5 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-500 rounded-full ">
+												<span className='absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1.5 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-500 rounded-full '>
 													{notifications.length}
 												</span>
 											) : null}
 										</CustomButton>
 									</PopoverTrigger>
-									<PopoverContent className="w-80 p-0">
-										<Notifications notifications={notifications} />
+									<PopoverContent className='w-[400px] p-0'>
+										<Notifications
+											notifications={notifications}
+										/>
 									</PopoverContent>
 								</Popover>
 								<Avatar>
-									<AvatarImage src={user.user_metadata.avatar_url} />
+									<AvatarImage
+										src={user.user_metadata.avatar_url}
+									/>
 									<AvatarFallback>
-										<Skeleton className="w-[100px] h-[20px] rounded-full" />
+										<Skeleton className='w-[100px] h-[20px] rounded-full' />
 									</AvatarFallback>
 								</Avatar>
 							</div>
@@ -205,9 +213,9 @@ export function NavigationBar({ user }: { user: User }) {
 					</NavbarContent>
 				</div>
 			) : (
-				<NavbarContent justify="end">
-					<NavbarItem className="hidden lg:flex">
-						<Link href="/login">Login</Link>
+				<NavbarContent justify='end'>
+					<NavbarItem className='hidden lg:flex'>
+						<Link href='/login'>Login</Link>
 					</NavbarItem>
 				</NavbarContent>
 			)}
@@ -217,15 +225,14 @@ export function NavigationBar({ user }: { user: User }) {
 						<NextUILink
 							color={
 								pathname === `/${item.toLocaleLowerCase()}`
-									? "primary"
-									: item === "Log Out"
-									  ? "danger"
-									  : "foreground"
+									? 'primary'
+									: item === 'Log Out'
+									? 'danger'
+									: 'foreground'
 							}
-							className="w-full"
+							className='w-full'
 							href={`/${item.toLowerCase()}`}
-							size="lg"
-						>
+							size='lg'>
 							{item}
 						</NextUILink>
 					</NavbarMenuItem>
