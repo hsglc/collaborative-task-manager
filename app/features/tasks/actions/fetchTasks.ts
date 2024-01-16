ba763cb9c9df328568c92f8d83bd8d0f7e6eb304
hsglc <huseyingulcuu@gmail.com>
2024-01-16 15:27:14 +0300

    refactor: categorized fetched tasks before return them

--- app/features/tasks/actions/fetchTasks.ts
+++ app/features/tasks/actions/fetchTasks.ts
@@ -1,9 +1,10 @@
-'use server';
+"use server";
 
-import { CookieOptions, createServerClient } from '@supabase/ssr';
-import { cookies } from 'next/headers';
+import { Task } from "@/types/tasks";
+import { CookieOptions, createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
-export async function fetchTasks(search: string) {
+export async function fetchTasks() {
 	const cookieStore = cookies();
 
 	const supabase = createServerClient(
@@ -18,62 +19,49 @@ export async function fetchTasks(search: string) {
 					cookieStore.set({ name, value, ...options });
 				},
 				remove(name: string, options: CookieOptions) {
-					cookieStore.set({ name, value: '', ...options });
+					cookieStore.set({ name, value: "", ...options });
 				},
 			},
-		}
+		},
 	);
 	const {
 		data: { session },
 	} = await supabase.auth.getSession();
 
 	const { data: tasks } = await supabase
-		.from('tasks')
-		.select('*')
-		.order('created_at', { ascending: false })
-		.or(
-			`created_by.eq.${session?.user.id},assignee.eq.${session?.user.id}`
-		);
-
-	const tasksWithHighPriority = tasks?.filter(
-		(task) => task.priority === 'High'
-	);
-	const completedTasks = tasks?.filter((task) => task.status === 'Done');
-	const uncompletedTasks = tasks?.filter((task) => task.status !== 'Done');
-	const assignedTasks = tasks?.filter(
+		.from("tasks")
+		.select("*")
+		.order("created_at", { ascending: false })
+		.or(`created_by.eq.${session?.user.id},assignee.eq.${session?.user.id}`)
+		.returns<Task[]>();
+	const myTasks = tasks?.filter(
 		(task) =>
-			task.assignee === session?.user.id &&
-			task.created_by !== session?.user.id
+			task.created_by === session?.user.id &&
+			task.assignee === session?.user.id,
 	);
-
-	const sharedTasks = tasks?.filter(
+	const sharedByOthers = tasks?.filter(
 		(task) =>
-			(task.assignee === session?.user.id &&
-				task.created_by !== session?.user.id) ||
-			(task.created_by === session?.user.id &&
-				task.assignee !== session?.user.id)
+			task.created_by === session?.user.id &&
+			task.assignee !== session?.user.id,
 	);
 
-	const filteredTasks =
-		search === 'all'
-			? tasks
-			: search === 'important'
-			? tasksWithHighPriority
-			: search === 'completed'
-			? completedTasks
-			: search === 'shared'
-			? sharedTasks
-			: uncompletedTasks;
+	const sharedWithMe = tasks?.filter(
+		(task) =>
+			task.created_by !== session?.user.id &&
+			task.assignee === session?.user.id,
+	);
+
+	if (!myTasks || !sharedByOthers || !sharedWithMe) {
+		return {
+			myTasks: [],
+			sharedByOthers: [],
+			sharedWithMe: [],
+		};
+	}
 
 	return {
-		tasks: filteredTasks,
-		numberOfTasks: {
-			tasksNumber: tasks?.length,
-			importantTasksNumber: tasksWithHighPriority?.length,
-			completedTasksNumber: completedTasks?.length,
-			uncompletedTasksNumber: uncompletedTasks?.length,
-			assignedTasksNumber: assignedTasks?.length,
-			sharedTasksNumber: sharedTasks?.length,
-		},
+		myTasks,
+		sharedByOthers,
+		sharedWithMe,
 	};
 }
