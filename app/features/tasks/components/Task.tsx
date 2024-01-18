8681c844dc40e7bb1afdee34844feb72c0ea9717
hsglc <huseyingulcuu@gmail.com>
2024-01-18 23:14:06 +0300

    feat: show a banner if there is no task

--- app/features/tasks/components/Task.tsx
+++ app/features/tasks/components/Task.tsx
@@ -1,31 +1,27 @@
-"use client";
+import { format, parseISO } from 'date-fns';
+import { Calendar } from 'lucide-react';
 
-import { format, parseISO } from "date-fns";
-import { Calendar } from "lucide-react";
-import { useState } from "react";
+import { Container } from '@/app/features/comments/components/Container';
+import { DeleteTaskDialog } from '@/app/features/deleteTask/components/DeleteTaskDialog';
+import { EditTaskDialog } from '@/app/features/editTask/components/EditTaskDialog';
+import { TaskPriority } from '@/app/features/taskPriority/components/TaskPriority';
 
-import { Container } from "@/app/features/comments/components/Container";
-import { DeleteTaskDialog } from "@/app/features/deleteTask/components/DeleteTaskDialog";
-import { EditTaskDialog } from "@/app/features/editTask/components/EditTaskDialog";
-import { TaskPriority } from "@/app/features/taskPriority/components/TaskPriority";
-
-import { Task as NewTask } from "@/types/tasks";
+import { Task as NewTask } from '@/types/tasks';
 
 type Props = {
 	task: NewTask;
 };
 
 export const Task = ({ task }: Props) => {
-	const [isCommentsOpen, setIsCommentsOpen] = useState(false);
 	const date = parseISO(task.created_at);
-	const formattedDate = format(date, "dd/MM/yyyy");
+	const formattedDate = format(date, 'dd/MM/yyyy');
 
 	return (
-		<div className="rounded-md p-4 text-black shadow-sm shadow-black space-y-3 ">
-			<h4 className="text-2xl font-semibold">{task.name}</h4>
-			<div className="flex-center justify-between">
+		<div className='rounded-md p-4 text-black shadow-sm shadow-black space-y-3 '>
+			<h4 className='text-2xl font-semibold'>{task.name}</h4>
+			<div className='flex-center justify-between'>
 				<p>{task.description}</p>
-				<div className="flex-center gap-4">
+				<div className='flex-center gap-4'>
 					<TaskPriority priority={task.priority} />
 
 					<EditTaskDialog task={task} />
@@ -34,7 +30,7 @@ export const Task = ({ task }: Props) => {
 					<Container taskId={task.id} />
 				</div>
 			</div>
-			<div className="flex-center gap-2">
+			<div className='flex-center gap-2'>
 				<Calendar />
 				{formattedDate}
 			</div>
--- app/features/tasks/components/Tasks.tsx
+++ app/features/tasks/components/Tasks.tsx
@@ -1,6 +1,7 @@
-import { Task } from "@/app/features/tasks/components/Task";
+import { Task } from '@/app/features/tasks/components/Task';
 
-import { Task as NewTask } from "@/types/tasks";
+import { Task as NewTask } from '@/types/tasks';
+import NoTask from './NoTask';
 
 type Props = {
 	tasks: NewTask[];
@@ -8,11 +9,13 @@ type Props = {
 
 export const Tasks = ({ tasks }: Props) => {
 	return (
-		<div className="col-span-6  p-4 space-y-8 max-h-max overflow-scroll">
-			<h2 className="text-xl font-medium">All Tasks ({tasks?.length})</h2>
-			{tasks?.map((task) => (
-				<Task key={task.id} task={task} />
-			))}
+		<div className='col-span-6  p-4 space-y-8 max-h-max overflow-scroll'>
+			<h2 className='text-xl font-medium'>All Tasks ({tasks?.length})</h2>
+			{tasks.length > 0 ? (
+				tasks?.map((task) => <Task key={task.id} task={task} />)
+			) : (
+				<NoTask />
+			)}
 		</div>
 	);
 };
