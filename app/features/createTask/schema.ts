cfd5e1e678fd05e7acf8bb5834c498c7160c05f3
hsglc <huseyingulcuu@gmail.com>
2024-01-23 22:49:49 +0300

    refactor: update character limits for form fields

--- app/features/createTask/schema.ts
+++ app/features/createTask/schema.ts
@@ -7,8 +7,8 @@ export const formSchema = z.object({
 		.min(2, {
 			message: "This field must be at least 2 characters long.",
 		})
-		.max(50, {
-			message: "This field must be at most 50 characters long.",
+		.max(80, {
+			message: "This field must be at most 80 characters long.",
 		}),
 	description: z
 		.string()
@@ -16,8 +16,8 @@ export const formSchema = z.object({
 		.min(2, {
 			message: "This field must be at least 2 characters long.",
 		})
-		.max(250, {
-			message: "This field must be at most 250 characters long.",
+		.max(350, {
+			message: "This field must be at most 350 characters long.",
 		}),
 	priority: z.string({
 		required_error: "This field is required.",
