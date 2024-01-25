2b53205a8e28f3086dec4b443018081d3c240f30
hsglc <huseyingulcuu@gmail.com>
2024-01-26 00:19:41 +0300

    refactor: refactor comments container component

--- app/features/comments/components/Container.tsx
+++ app/features/comments/components/Container.tsx
@@ -1,51 +1,49 @@
-"use client";
+'use client';
 
-import { createBrowserClient } from "@supabase/ssr";
-import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
-
-import {
-	Button,
-	Modal,
-	ModalBody,
-	ModalContent,
-	ModalFooter,
-	ModalHeader,
-	useDisclosure,
-} from "@nextui-org/react";
+import { createBrowserClient } from '@supabase/ssr';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
 import {
 	Avatar,
 	AvatarFallback,
 	AvatarImage,
-} from "@/app/components/ui/avatar";
-import { Skeleton } from "@/app/components/ui/skeleton";
+} from '@/app/components/ui/avatar';
+import { Skeleton } from '@/app/components/ui/skeleton';
 
-import { ScrollArea } from "@/app/components/ui/scroll-area";
-import { Separator } from "@/app/components/ui/separator";
-import { formatRelativeTime } from "@/app/lib/utils";
-import type { Comment } from "@/types/comments";
-import { AddCommentForm } from "./AddCommentForm";
+import { ScrollArea } from '@/app/components/ui/scroll-area';
+import { Separator } from '@/app/components/ui/separator';
+import { formatRelativeTime } from '@/app/lib/utils';
+import type { Comment } from '@/types/comments';
+import { AddCommentForm } from './AddCommentForm';
+import { Button } from '@/app/components/ui/button';
 
 type Props = {
 	taskId: number;
 };
 
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from '@/app/components/ui/dialog';
+
 export function Container({ taskId }: Props) {
 	const { push } = useRouter();
-	const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
 	const [comments, setComments] = useState<Comment[]>([]);
 
 	const supabase = createBrowserClient(
 		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 	);
 
 	const fetchComments = async () => {
-		onOpen();
 		const { data: comments, error } = await supabase
-			.from("comments")
+			.from('comments')
 			.select(
 				`
                 id,
@@ -59,9 +57,9 @@ export function Container({ taskId }: Props) {
                     full_name
                   )
             
-            `,
+            `
 			)
-			.eq("task_id", taskId)
+			.eq('task_id', taskId)
 			.returns<Comment[]>();
 
 		if (error) console.log(error);
@@ -70,13 +68,13 @@ export function Container({ taskId }: Props) {
 
 	useEffect(() => {
 		const channels = supabase
-			.channel("custom-insert-channel")
+			.channel('custom-insert-channel')
 			.on(
-				"postgres_changes",
-				{ event: "INSERT", schema: "public", table: "comments" },
+				'postgres_changes',
+				{ event: 'INSERT', schema: 'public', table: 'comments' },
 				(payload) => {
 					fetchComments();
-				},
+				}
 			)
 			.subscribe();
 
@@ -87,68 +85,72 @@ export function Container({ taskId }: Props) {
 
 	return (
 		<>
-			<Button color="primary" variant="bordered" onPress={fetchComments}>
-				Comments
-			</Button>
-			<Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-				<ModalContent>
-					{(onClose) => (
-						<>
-							<ModalHeader className="flex flex-col gap-1">
-								Comments
-							</ModalHeader>
-							<ModalBody>
-								<ScrollArea className="h-96">
-									{comments.length > 0 ? (
-										comments.map((comment) => (
-											<React.Fragment key={comment.id}>
-												<div className="flex flex-col gap-1">
-													<div className="flex flex-col gap-2">
-														<div>
-															<div className="flex gap-2">
-																<Avatar>
-																	<AvatarImage
-																		src={comment.profiles.avatar_url}
-																	/>
-																	<AvatarFallback>
-																		<Skeleton className="w-[40px] h-[20px] rounded-full" />
-																	</AvatarFallback>
-																</Avatar>
-																<div className="flex flex-col justify-between">
-																	<div className="flex gap-3 items-center">
-																		<p className="text-medium font-bold">
-																			{comment.profiles.full_name}
-																		</p>
-																		<p className="text-gray-400 text-xs">
-																			{formatRelativeTime(
-																				new Date(comment.created_at),
-																			)}
-																		</p>
-																	</div>
-																	<p>{comment.comment}</p>
-																</div>
-															</div>
+			<Dialog>
+				<DialogTrigger asChild>
+					<Button color='primary' onClick={fetchComments}>
+						Comments
+					</Button>
+				</DialogTrigger>
+				<DialogContent className='sm:max-w-[425px]'>
+					<DialogHeader>
+						<DialogTitle>Comments</DialogTitle>
+						<DialogDescription>
+							Comments are a great way to collaborate with your
+							team.
+						</DialogDescription>
+					</DialogHeader>
+					<ScrollArea className='h-96'>
+						{comments.length > 0 ? (
+							comments.map((comment) => (
+								<React.Fragment key={comment.id}>
+									<div className='flex flex-col gap-1'>
+										<div className='flex flex-col gap-2'>
+											<div>
+												<div className='flex gap-2'>
+													<Avatar>
+														<AvatarImage
+															src={
+																comment.profiles
+																	.avatar_url
+															}
+														/>
+														<AvatarFallback>
+															<Skeleton className='w-[40px] h-[20px] rounded-full' />
+														</AvatarFallback>
+													</Avatar>
+													<div className='flex flex-col justify-between'>
+														<div className='flex gap-3 items-center'>
+															<p className='text-medium font-bold'>
+																{
+																	comment
+																		.profiles
+																		.full_name
+																}
+															</p>
+															<p className='text-gray-400 text-xs'>
+																{formatRelativeTime(
+																	new Date(
+																		comment.created_at
+																	)
+																)}
+															</p>
 														</div>
+														<p>{comment.comment}</p>
 													</div>
 												</div>
-												<Separator className="my-2" />
-											</React.Fragment>
-										))
-									) : (
-										<p>There are no comments yet.</p>
-									)}
-								</ScrollArea>
-								<AddCommentForm taskId={taskId} />
-							</ModalBody>
-							<ModalFooter>
-								<Button color="danger" variant="light" onPress={onClose}>
-									Close
-								</Button>
-							</ModalFooter>
-						</>
-					)}
-				</ModalContent>
-			</Modal>
+											</div>
+										</div>
+									</div>
+									<Separator className='my-2' />
+								</React.Fragment>
+							))
+						) : (
+							<p>There are no comments yet.</p>
+						)}
+					</ScrollArea>
+					<AddCommentForm taskId={taskId} />
+				</DialogContent>
+			</Dialog>
 		</>
 	);
 }
