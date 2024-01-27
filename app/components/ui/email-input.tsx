cc98a0660aae309f1f9fd5cea4203d1d2f0dc8b8
hsglc <huseyingulcuu@gmail.com>
2024-01-27 23:43:38 +0300

    feat: update shared Navbar and add EmailInput and PasswordInput components

---
+++ app/components/ui/email-input.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Button } from "@/app/components/ui/button";
+import { Input, InputProps } from "@/app/components/ui/input";
+import { cn } from "@/app/lib/shared";
+import { MailIcon } from "lucide-react";
+import { forwardRef, useState } from "react";
+
+const EmailInput = forwardRef<HTMLInputElement, InputProps>(
+	({ className, ...props }, ref) => {
+		const [showPassword, setShowPassword] = useState(false);
+
+		return (
+			<div className="relative">
+				<Input
+					type="email"
+					name="email"
+					id="email"
+					className={cn("hide-password-toggle pl-10", className)}
+					ref={ref}
+					{...props}
+				/>
+				<Button
+					type="button"
+					variant="ghost"
+					className="absolute  top-0 h-full px-3 py-2"
+				>
+					<MailIcon color="#aca0a0" />
+				</Button>
+			</div>
+		);
+	},
+);
+EmailInput.displayName = "PasswordInput";
+
+export { EmailInput };
---
+++ app/components/ui/password-input.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Button } from "@/app/components/ui/button";
+import { Input, InputProps } from "@/app/components/ui/input";
+import { cn } from "@/app/lib/shared";
+import { EyeIcon, EyeOffIcon, LockIcon } from "lucide-react";
+import { forwardRef, useState } from "react";
+
+const PasswordInput = forwardRef<HTMLInputElement, InputProps>(
+	({ className, ...props }, ref) => {
+		const [showPassword, setShowPassword] = useState(false);
+
+		return (
+			<div className="relative">
+				<Input
+					id="password"
+					name="password"
+					type={showPassword ? "text" : "password"}
+					className={cn("hide-password-toggle px-10", className)}
+					ref={ref}
+					{...props}
+				/>
+				<Button
+					type="button"
+					variant="ghost"
+					className="absolute top-0 h-full px-3 py-2"
+				>
+					<LockIcon color="#aca0a0" />
+				</Button>
+				<Button
+					type="button"
+					variant="ghost"
+					size="sm"
+					className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+					onClick={() => setShowPassword((prev) => !prev)}
+				>
+					{showPassword ? (
+						<EyeIcon className="h-4 w-4" aria-hidden="true" />
+					) : (
+						<EyeOffIcon className="h-4 w-4" aria-hidden="true" />
+					)}
+					<span className="sr-only">
+						{showPassword ? "Hide password" : "Show password"}
+					</span>
+				</Button>
+
+				{/* hides browsers password toggles */}
+				<style>{`
+					.hide-password-toggle::-ms-reveal,
+					.hide-password-toggle::-ms-clear {
+						visibility: hidden;
+						pointer-events: none;
+						display: none;
+					}
+				`}</style>
+			</div>
+		);
+	},
+);
+PasswordInput.displayName = "PasswordInput";
+
+export { PasswordInput };
