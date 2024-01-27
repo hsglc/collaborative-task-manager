bb2e063dfd2afeda9f5af446fdfb280ec31ddf9e
hsglc <huseyingulcuu@gmail.com>
2024-01-27 23:41:40 +0300

    refactor: delete unused login components and add new authentication route

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,24 +1,19 @@
-"use client";
-import { Button, Divider } from "@nextui-org/react";
-
 import SocialloginButton from "@/app/components/SocialLoginButton";
-import { createBrowserClient } from "@supabase/ssr";
+import { Button } from "@/app/components/ui/button";
+import { EmailInput } from "@/app/components/ui/email-input";
+import { PasswordInput } from "@/app/components/ui/password-input";
+import { Separator } from "@/app/components/ui/separator";
+import { createClient } from "@/app/lib/supabase/server";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import { Suspense } from "react";
-import MailInput from "./mailInput";
-import Messages from "./messages";
-import PasswordInput from "./passwordInput";
+import { login, signup } from "./actions";
 
 export default async function Login() {
-	const supabase = createBrowserClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-	);
+	const cookieStore = cookies();
+	const supabase = createClient(cookieStore);
 
-	const {
-		data: { session },
-	} = await supabase.auth.getSession();
-	if (session) {
+	const { data, error } = await supabase.auth.getUser();
+	if (!error || data?.user) {
 		redirect("/dashboard");
 	}
 
@@ -29,33 +24,31 @@ export default async function Login() {
 				<SocialloginButton provider="github" />
 			</div>
 			<div className="flex items-center justify-between">
-				<Divider className="w-[210px]" />
+				<Separator className="w-[210px]" />
 				<p>or sign up with email</p>
-				<Divider className="w-[210px]" />
+				<Separator className="w-[210px]" />
 			</div>
-			<form
-				className=" flex flex-col w-full justify-center gap-3 text-foreground"
-				action="/auth/sign-in"
-				method="post"
-			>
-				<MailInput />
-				<PasswordInput />
+			<form className=" flex flex-col w-full justify-center gap-3 text-foreground">
+				<EmailInput autoComplete="email" placeholder="Email" />
+				<PasswordInput autoComplete="password" placeholder="Password" />
 
-				<Button color="primary" variant="solid" type="submit">
+				<Button
+					color="primary"
+					variant="black"
+					type="submit"
+					formAction={login}
+				>
 					Sign In
 				</Button>
 
 				<Button
 					type="submit"
-					formAction="/auth/sign-up"
+					formAction={signup}
 					color="primary"
-					variant="bordered"
+					variant="black"
 				>
 					Sign Up
 				</Button>
-				<Suspense fallback={<div>Loading...</div>}>
-					<Messages />
-				</Suspense>
 			</form>
 		</div>
 	);
