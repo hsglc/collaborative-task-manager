64f9f46776bb570a0e682ed3026e495f34645420
hsglc <huseyingulcuu@gmail.com>
2024-01-27 23:42:40 +0300

    feat: add Supabase client and check user authentication

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { createClient } from "@/app/lib/supabase/server";
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
@@ -18,23 +19,10 @@ export default async function DashboardIndex({
 	searchParams: searchParams;
 }) {
 	const cookieStore = cookies();
+	const supabase = createClient(cookieStore);
 
-	const supabase = createServerClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-		{
-			cookies: {
-				get(name: string) {
-					return cookieStore.get(name)?.value;
-				},
-			},
-		},
-	);
-
-	const {
-		data: { session },
-	} = await supabase.auth.getSession();
-	if (!session) {
+	const { data, error } = await supabase.auth.getUser();
+	if (error || !data?.user) {
 		redirect("/login");
 	}
 
