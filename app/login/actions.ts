6becd303edf0b5fcabe08c8a46f4c4e5e7e3b2cb
hsglc <huseyingulcuu@gmail.com>
2024-02-15 22:42:54 +0300

    refactor: refactor login actions and add e2e test script

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -31,16 +31,12 @@ export async function signup(formData: FormData) {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get("email") as string,
     password: formData.get("password") as string,
   };
-  console.log("data :", data);
 
   const { error } = await supabase.auth.signUp(data);
-  console.log("error :", error);
 
   if (error) {
     redirect("/error");
